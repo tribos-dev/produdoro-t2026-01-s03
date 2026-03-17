6dac879ea52c170fc60f7d28a5ed9ac04fb961d8
Onofrijv <jvsilvaonofri@gmail.com>
2026-03-17 13:10:50 -0300

    feat/implementa controller

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaListResponse.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaListResponse.java
@@ -1,7 +1,23 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
 import lombok.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
 
 @Value
 public class TarefaListResponse {
+    private UUID idTarefa;
+    private String descricao;
+    private UUID idUsuario;
+    private UUID idArea;
+    private UUID idProjeto;
+    private StatusTarefa status;
+    private StatusAtivacaoTarefa statusAtivacao;
+    private int contagemPomodoro;
+    private int posicaoTarefa;
 }
--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioService.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioService.java
@@ -1,12 +1,15 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaListResponse;
 import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioCriadoResponse;
 import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UsuarioService {
 	UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
     UsuarioCriadoResponse buscaUsuarioPorId(UUID idUsuario);
 
+    List<TarefaListResponse> listaTodasTarefas();
 }
