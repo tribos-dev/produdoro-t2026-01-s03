ee4a96e95c57835015fdfa06b2f1a86a7f90d438
DevMatheusMoura <matheussilvamouras@gmail.com>
2026-03-23 00:59:04 -0300

    test: Implementa teste unitário.

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,7 +1,38 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
 class UsuarioApplicationServiceTest {
+    @InjectMocks
+    private UsuarioApplicationService usuarioApplicationService;
 
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    void mudaStatusParaPausaCurta () {
+        Usuario usuario = DataHelper.createUsuario() ;
+        when(usuarioRepository.buscaUsuarioPorEmail(usuario.getEmail())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorId(usuario.getIdUsuario())).thenReturn(usuario);
+        usuarioApplicationService.mudaStatusParaPausaCurta(usuario.getEmail(), usuario.getIdUsuario());
+        assertEquals(StatusUsuario.PAUSA_CURTA, usuario.getStatus());
+        verify(usuarioRepository).buscaUsuarioPorEmail(usuario.getEmail());
+        verify(usuarioRepository).buscaUsuarioPorId(usuario.getIdUsuario());
+        verify(usuarioRepository).salva(usuario);
+
+
+    }
 }
