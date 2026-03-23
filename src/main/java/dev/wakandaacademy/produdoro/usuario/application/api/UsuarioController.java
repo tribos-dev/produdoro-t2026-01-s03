5a98d4adfdfb69e4b2871cd131da5a47d3afa6db
Onofrijv <jvsilvaonofri@gmail.com>
2026-03-23 15:24:28 -0300

    style/elimina-imports-desnecessarios-em-usuario

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
@@ -2,7 +2,6 @@ package dev.wakandaacademy.produdoro.usuario.application.api;
 
 import javax.validation.Valid;
 
-import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaListResponse;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +9,6 @@ import dev.wakandaacademy.produdoro.usuario.application.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
