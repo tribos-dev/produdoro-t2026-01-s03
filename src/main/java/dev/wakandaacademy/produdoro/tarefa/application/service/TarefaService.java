0e42d6937243e696d4bafc7880dbf1b5949d014d
Onofrijv <jvsilvaonofri@gmail.com>
2026-03-23 19:43:59 -0300

    feat: implementa service

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 public interface TarefaService {
     TarefaIdResponse criaNovaTarefa(TarefaRequest tarefaRequest);
     Tarefa detalhaTarefa(String usuario, UUID idTarefa);
+
+    void deletaTodasTarefas(String usuario, UUID idUsuario);
 }
