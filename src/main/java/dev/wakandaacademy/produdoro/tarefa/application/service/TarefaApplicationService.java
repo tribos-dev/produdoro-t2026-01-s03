97fec837eb528d3d4e2e0ceedb8a49226c887f15
Celson Jardim <celsonjardim.dev@outlook.com>
2026-03-23 20:42:25 -0300

    feat: Adiciona validacao se tareja ja esta concuida

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationService.java
@@ -4,6 +4,7 @@ import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
 import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
@@ -45,6 +46,9 @@ public class TarefaApplicationService implements TarefaService {
     public void concluiTarefa(String usuario, UUID idTarefa) {
         log.info("[inicia] TarefaApplicationService - concluiTarefa");
         Tarefa tarefa = detalhaTarefa(usuario, idTarefa);
+        if (tarefa.getStatus() == StatusTarefa.CONCLUIDA) {
+            throw APIException.build(HttpStatus.BAD_REQUEST,"Tarefa já está concuída");
+        }
         tarefa.mudaStatusParaConcluida();
         tarefaRepository.salva(tarefa);
         log.info("[finaliza] TarefaApplicationService - concluiTarefa");
